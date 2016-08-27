a316f79768c4619196a108005c2eebf7e1aa5fbb
Gregor Tudan <gregor.tudan@cofinpro.de>
2016-08-27 11:55:57 +0200

    added multichain component

--- angular-client/client/app/common/common.js
+++ angular-client/client/app/common/common.js
@@ -2,13 +2,15 @@ import angular from 'angular';
 import Navbar from './navbar/navbar';
 import Hero from './hero/hero';
 import User from './user/user';
+import Multichain from './multichain/multichain'
 
 let commonModule = angular.module('app.common', [
   Navbar,
   Hero,
-  User
+  User,
+  Multichain
 ])
-  
+
 .name;
 
 export default commonModule;
